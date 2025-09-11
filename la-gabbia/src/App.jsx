6d55564ee296de22aaa8b9b3b67812552d81dd43
yilmaz1998 <snn.ylmz9804@gmail.com>
2025-09-11 11:51:40 -0400

    React Router set up

--- la-gabbia/src/App.jsx
+++ la-gabbia/src/App.jsx
@@ -1,6 +1,23 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+
+import Menu from "./routes/Menu"
+import Cart from "./routes/Cart"
+import CompleteOrder from "./routes/CompleteOrder"
+import Orders from "./routes/Orders"
+
+import Header from "./components/Header"
+
 const App = () => {
   return (
-    <div className="text-5xl">App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/complete-order" element={<CompleteOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
