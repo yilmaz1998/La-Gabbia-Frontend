def5e2d4b53f7503b3e65e599cbbd7baa7efa1a1
yilmaz1998 <snn.ylmz9804@gmail.com>
2025-10-02 13:46:12 -0400

    Order page styling

--- la-gabbia/src/routes/Orders.jsx
+++ la-gabbia/src/routes/Orders.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { axiosInstance } from '../lib/axios'
+import Button from '@mui/material/Button';
+import { IoFastFoodOutline } from "react-icons/io5";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
@@ -20,8 +22,8 @@ const Orders = () => {
         setLoading(false)
       }
     }
-    fetchOrders() 
-}, [])
+    fetchOrders()
+  }, [])
 
   const handleLogOut = () => {
     localStorage.removeItem("adminToken");
@@ -30,8 +32,12 @@ const Orders = () => {
 
   return (
     <div>
-      <button onClick={handleLogOut}>Log Out</button>
-      <h1>Orders</h1>
+      <header className="flex px-4 py-4 items-center justify-between bg-gray-900">
+        <h1 className="text-3xl text-white flex gap-1"><IoFastFoodOutline />La Gabbia</h1>
+        <Button onClick={handleLogOut}>Log Out</Button>
+      </header>
+      <div className='p-4'>
+      <h1 className='text-center mt-8 text-4xl'>Orders</h1>
       {loading ? (
         <p>Loading...</p>
       ) : (
@@ -39,24 +45,28 @@ const Orders = () => {
           {orders.length === 0 ? (
             <p>No orders found.</p>
           ) : orders.map(order => (
-            <div key={order.id}>
-              <h2>Order ID: {order.id}</h2>
-              <p>Name: {order.customer_name}</p>
-              <p>Address: {order.customer_address}</p>
-              <p>Total: ${order.total_price}</p>
-              <p>Created: {new Date(order.created_at).toLocaleString()}</p>
-              <h3>Items:</h3>
+            <div key={order.id} className='mt-6 border rounded px-4 py-4'>
+              <h2 className="text-lg font-semibold text-blue-800">Order ID: {order.id}</h2>
+              <p><span className="font-semibold text-blue-800">Name:</span> {order.customer_name}</p>
+              <p><span className="font-semibold text-blue-800">Address:</span> {order.customer_address}</p>
+              <p><span className="font-semibold text-blue-800">Email:</span> {order.customer_email}</p>
+              <p><span className="font-semibold text-blue-800">Total:</span> ${order.total_price}</p>
+              <p><span className="font-semibold text-blue-800">Created:</span> {new Date(order.created_at).toLocaleString()}</p>
+              <h3 className='font-semibold text-blue-800'>Items:</h3>
               <ul>
                 {order.items.map(item => (
                   <li key={item.id}>
-                    {item.name} - Quantity: {item.quantity} - Instructions: {item.instruction || 'None'}
+                    {item.quantity} x {item.name} {item.instruction && `(${item.instruction})`}
                   </li>
                 ))}
               </ul>
+              <Button>Update Status</Button>
+              <Button>Delete Order</Button>
             </div>
           ))}
         </div>
       )}
+    </div>
     </div>
   )
 }
